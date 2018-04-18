0e8868dacae6f2998e5f081bcd4c9e6c9336e16b
Nima <nim4.karimi@gmail.com>
2018-04-18 23:51:53 +0430

    Add Form Django model

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 # def home_page(request):
 #     return HttpResponse('<h1>eCommerce</h1>')
 
@@ -19,14 +21,18 @@ def about_page(request):
     return render(request, 'home_page.html', context)
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
         'title':'contact page',
-        'content':'contact eCommerce'
+        'content':'contact eCommerce',
+        'form':contact_form
     }
-    if request.method=='POST':
-        #print(request.POST)
-        print(request.POST.get('fullname'))
-        print(request.POST.get('email'))
-        print(request.POST.get('content'))
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method=='POST':
+    #     #print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
 
     return render(request, 'contact/view.html', context)
